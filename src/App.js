0d3b11c45ba1e183b39f4080c518795638146c53
lramirez-js <luis.rg@outlook.com>
2022-10-11 02:26:57 -0500

    Component Section (gray card) added

--- src/App.js
+++ src/App.js
@@ -8,11 +8,20 @@ const Container = styled.div`
   height: 100%;
   align-items: center;
 `
+const Section = styled.section`
+  background-color: #eee;
+  border-top: solid 2px palevioletred;
+  padding: 20px 25px;
+  width: 500px;
+  box-shadow: 0px 2px 3px rgb(0,0,0,0.3);
+`
 
 const App = () => {
   return (
     <Container>
-      Hello World!
+      <Section>
+        Hello
+      </Section>
     </Container>
   )
 }
