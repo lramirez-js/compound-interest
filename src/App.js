d07b1a238b8cef67130b622a11ea609cfd3d71fa
lramirez-js <luis.rg@outlook.com>
2022-10-13 10:37:27 -0500

    Button Component added and refactor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import { Formik, Form } from 'formik'
-import styled from 'styled-components'
 import Input from './components/Input'
-import './App.css'
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  height: 100%;
-  align-items: center;
-`
-const Section = styled.section`
-  background-color: #eee;
-  border-top: solid 2px palevioletred;
-  padding: 20px 25px;
-  width: 500px;
-  box-shadow: 0px 2px 3px rgb(0,0,0,0.3);
-`
+import Button from './components/Button'
+import Container from './components/Container'
+import Section from './components/Section'
 
 const App = () => {
   const handleSubmit = (e) => {
@@ -39,6 +26,7 @@ const App = () => {
             <Input name="contribution" label="Annual contribution" />
             <Input name="years" label="Years" />
             <Input name="rate" label="Estimated rate" />
+            <Button>Calculate</Button>
           </Form>
         </Formik>
       </Section>
