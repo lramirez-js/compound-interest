7ac9698a40c34ab138e716ad76be052cdff3ff88
lramirez-js <luis.rg@outlook.com>
2022-10-22 06:09:40 -0500

    Showing the balance on the DOM

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { useState } from 'react'
 import { Formik, Form } from 'formik'
 import Input from './components/Input'
 import Button from './components/Button'
 import Container from './components/Container'
 import Section from './components/Section'
+import Balance from './components/Balance'
+import { styled } from 'styled-components';
 
 const compoundInterest = (deposit, contribution, years, rate) => {
   let total = deposit
@@ -14,10 +17,18 @@ const compoundInterest = (deposit, contribution, years, rate) => {
   return Math.round(total)
 }
 
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'UDS',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+})
+
 const App = () => {
+  const [balance, setBalance] = useState('')
   const handleSubmit = ({deposit, contribution, years, rate}) => {
     const val = compoundInterest(Number(deposit), Number(contribution), Number(years), Number(rate))
-    console.log(val)
+    setBalance(formatter.format(val))
   }
 
   return (
@@ -40,6 +51,7 @@ const App = () => {
             <Button>Calculate</Button>
           </Form>
         </Formik>
+        { balance !== '' ? <Balance>Final Balance: {balance}</Balance> : null }
       </Section>
     </Container>
   )
---
+++ src/components/Balance.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Balance = styled.section`
+  font-size: 36px;
+  margin-top: 12px;
+  display: inline-block
+  color: black
+`
+
+export default Balance
